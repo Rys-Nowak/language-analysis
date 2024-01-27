d19e2801a3ba0fccc18e89a0101ed2d6b5f6e830
Rys-Nowak <ryszard.nowak012@gmail.com>
2024-01-27 15:12:04 +0100

    readme

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-# language-analysis
+# Prównanie języka czeskiego do języka manuskryptu Wojnicza
 
-Voynich manuscrypt comparison to Chech language
+## Prawo Zipfa
+Gdy na podstawie ich korpusów językowych ustali się wykaz wyrazów ułożonych w malejącym porządku częstotliwości ich występowania, to ranga (numer porządkowy) wyrazu jest odwrotnie proporcjonalna do częstotliwości, zatem iloczyn częstotliwości i rangi powinien być wielkością stałą.
+
+### Wyniki analizy dla najczęściej występujących 300 wyrazów
+
+#### Częstotliwość występowania słów o kolejnych rangach - wykres powinien być hiperboliczny
+
+<img src="out/czech/zipf_300.jpg">
+
+Język czeski
+<hr>
+
+<img src="out/voynich/zipf_300.jpg">
+
+Język z manuskryptu
+
+#### Iloczyn częstotliwość występowania słów do ich rangi - wykres powinien być zbliżony do funckji stałej
+
+<img src="out/czech/zipf_coef_300.jpg">
+
+Język czeski
+<hr>
+
+<img src="out/voynich/zipf_coef_300.jpg">
+
+Język z manuskryptu
+
+### Wniosek
+Wykresy wskazują że dla słów o rangach 50-300 prawo Zipfa dla obu języków jest dobrze spełnione - odchylenia od stałej wartości są niewielkie. W przypadkuj języka z manuskryptu wojnicza, z pominięciem kilku najczęściej występujących słów, są one zauważalnie mniejsze niż w analizowanym czeskim tekście
--- zipf_law.py
+++ zipf_law.py
@@ -22,28 +22,31 @@ def create_zipf_dict(words: List[str]):
 
 def plot(x, y, xlabel, ylabel, path):
     plt.figure()
-    plt.plot(x, y)
+    plt.ylim(0 - max(y) / 20, max(y) + max(y) / 20)
+    plt.grid()
+    plt.plot(x, y, '.')
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
+    plt.title(path)
     plt.savefig(os.path.join(OUT_PATH, path))
 
 
 def plot_zipf(words: Dict[str, int], path):
     ranks = np.arange(1, len(words) + 1)
-    values = np.array(list(words.values()))
+    frequency = np.array(list(words.values())) / sum(words.values())
 
-    plot(ranks, values, "rank", "occurencies",
+    plot(ranks, frequency, "rank", "frequency",
          os.path.join(path, "zipf_all.jpg"))
-    plot(ranks, np.multiply(values, ranks), "rank", "rank*occurencies",
+    plot(ranks, np.multiply(frequency, ranks), "rank", "rank*frequency",
          os.path.join(path, "zipf_coef_all.jpg"))
-    plot(ranks[:300], values[:300], "rank", "occurencies",
+    plot(ranks[:300], frequency[:300], "rank", "frequency",
          os.path.join(path, "zipf_300.jpg"))
-    plot(ranks[:300], np.multiply(values[:300], ranks[:300]), "rank",
-         "rank*occurencies", os.path.join(path, "zipf_coef_300.jpg"))
-    plot(ranks[:100], values[:100], "rank", "occurencies",
+    plot(ranks[:300], np.multiply(frequency[:300], ranks[:300]), "rank",
+         "rank*frequency", os.path.join(path, "zipf_coef_300.jpg"))
+    plot(ranks[:100], frequency[:100], "rank", "frequency",
          os.path.join(path, "zipf_100.jpg"))
-    plot(ranks[:100], np.multiply(values[:100], ranks[:100]), "rank",
-         "rank*occurencies", os.path.join(path, "zipf_coef_100.jpg"))
+    plot(ranks[:100], np.multiply(frequency[:100], ranks[:100]), "rank",
+         "rank*frequency", os.path.join(path, "zipf_coef_100.jpg"))
 
 
 def read_voynich_words() -> List[str]:
